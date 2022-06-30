7440699ac953b63d9e9b8ff813ff1a211c22e239
mauricio <mauriciom.curiel@gmail.com>
2022-06-30 23:47:53 +0000

    changes datatypes of attributes for better modelling and adds arguments to new AutoValue_Trip() call to satisfy it

--- src/main/java/com/google/sps/model/Trip.java
+++ src/main/java/com/google/sps/model/Trip.java
@@ -1,18 +1,21 @@
 package com.google.sps.model;
 
 import com.google.auto.value.AutoValue;
-import java.util.List;
 
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.Set;
+D
 @AutoValue
 public abstract class Trip {
 
     static Trip create(long tripID, String title, float totalBudget){
-        return new AutoValue_Trip(tripID, title, totalBudget);
+        return new AutoValue_Trip(tripID, title, new HashSet<Long>(), new TreeSet<Long>(), 0.0F, totalBudget);
     }
     abstract long tripID();
     abstract String title();
-    abstract List<Long> participants();
-    abstract List<Long> eventIds();
+    abstract Set<Long> participants();
+    abstract Set<Long> eventIds();
     abstract float totalCost();
     abstract float totalBudget();
 }
