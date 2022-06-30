9aa6bd60ed704cfdace267232e7e5d7d1940f06a
Zhe He <jackhe313@g.ucla.edu>
2022-06-30 02:36:32 +0000

    basic implementation of add new trip to datastore

--- src/main/java/com/google/sps/model/Trip.java
+++ src/main/java/com/google/sps/model/Trip.java
@@ -3,17 +3,9 @@ package com.google.sps.model;
 import java.util.List;
 
 public class Trip {
-    private long tripID;
-    private String title;
-    private List<Long> participants;
+    public long tripID;
+    public String title;
+    public List<Long> participants;
     public float totalCost;
-    private float totalBudget;
-    
-    public long getParticipantAmmountOwed(long userId){
-        //TODO: calculate participant ammount owed
-    }
-
-    public long getParticipantAmmountPaid(long userId){
-        //TODO: calculate ammount paid  by a user
-    }
+    public float totalBudget;
 }
