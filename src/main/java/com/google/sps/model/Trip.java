de1121ba991a09e14813c910e3840bbdbf884ace
mauricio <mauriciom.curiel@gmail.com>
2022-07-01 01:55:08 +0000

    updates Trip eventIDs to use HashSet

--- src/main/java/com/google/sps/model/Trip.java
+++ src/main/java/com/google/sps/model/Trip.java
@@ -3,14 +3,13 @@ package com.google.sps.model;
 import com.google.auto.value.AutoValue;
 
 import java.util.HashSet;
-import java.util.TreeSet;
 import java.util.Set;
 
 @AutoValue
 public abstract class Trip {
 
     static Trip create(long tripID, String title, float totalBudget){
-        return new AutoValue_Trip(tripID, title, new HashSet<Long>(), new TreeSet<Long>(), 0.0F, totalBudget);
+        return new AutoValue_Trip(tripID, title, new HashSet<Long>(), new HashSet<Long>(), 0.0F, totalBudget);
     }
     abstract long tripID();
     abstract String title();
