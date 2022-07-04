cea3ffa2ad82ece089524b678fcb20d70fbf98f5
Zhe He <jackhe313@g.ucla.edu>
2022-07-04 07:52:18 +0000

    fix date parsing error, now the servlet can correctly parsing date input

--- src/main/java/com/google/sps/util/DataStoreHelper.java
+++ src/main/java/com/google/sps/util/DataStoreHelper.java
@@ -2,13 +2,16 @@ package com.google.sps.util;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import java.util.Date;
 
 public class DataStoreHelper {
     static public Date parseInputDate(String textDate){
         Date date;
         try {
-            date = DateFormat.getDateInstance().parse(textDate);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            date = dateFormat.parse(textDate);
         } catch (ParseException e) {
             return null;
         }
