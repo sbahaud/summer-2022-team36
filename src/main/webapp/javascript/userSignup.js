09ede7315e191fedc5dc47ad526f98ea13a66ecb
Emma Yuan <jinyueyu@usc.edu>
2022-07-06 11:20:15 -0700

    connect user signin frontend to backend

---
+++ src/main/webapp/javascript/userSignup.js
@@ -0,0 +1,65 @@
+// input validator
+function input_check(input){
+    var letters = /^[\w \-']+$/;
+    if(letters.test(input)){
+        console.log("passed check");
+        return true;
+    } else {
+        console.log("illegal input");
+        return false;
+    }
+}
+
+// send username to backend
+function postUsername(username) {
+    const params = new URLSearchParams();
+
+    params.append('USER_NAME_PARAM', username);
+
+    fetch('/SignUp', {method: 'POST', body: params})
+}
+
+
+// show/hide message
+var errorMsg = document.getElementById("error-msg");
+var emptyMsg = document.getElementById("empty-msg");
+var successMsg = document.getElementById("success-msg");
+
+function show(msg){
+    if(msg.classList.contains("hide")){
+        msg.classList.remove("hide");
+    }
+}
+
+function hide(msg){
+    if(!msg.classList.contains("hide")){
+        msg.classList.add("hide");
+    }
+}
+
+
+// get username
+var username = document.getElementById("signup-user-name");
+
+username.addEventListener("keypress", function(event) {
+    if(event.key === "Enter") {
+        event.preventDefault();
+        if(username.value === "") {
+            hide(errorMsg);
+            show(emptyMsg);
+        } else if(input_check(username.value)){
+            // console.log(username.value);
+            hide(errorMsg);
+            hide(emptyMsg);
+            show(successMsg);
+            postUsername(username.value);
+        } else {
+            // console.log("throw error message");
+            hide(emptyMsg);
+            show(errorMsg);
+        }
+
+        // clear input box
+        username.value = "";
+    }
+});
