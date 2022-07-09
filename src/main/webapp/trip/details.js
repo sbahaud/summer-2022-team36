239c4f5c981aa7e532763b6309343b5e8e6c5aa7
Yifan <sueyifankk@gmail.com>
2022-07-09 23:43:42 +0000

    made drop-down list as a function in details.js

--- src/main/webapp/trip/details.js
+++ src/main/webapp/trip/details.js
@@ -1,15 +1,17 @@
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
+function select() {
+    const selected = document.querySelector(".selected");
+    const optionsContainer = document.querySelector(".options-container");
 
-const optionsList = document.querySelectorAll(".option");
+    const optionsList = document.querySelectorAll(".option");
 
-selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-});
+    selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+    });
 
-optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-  });
-});
+    optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+    });
+}
