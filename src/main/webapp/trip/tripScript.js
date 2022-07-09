5ac4a5f987907c4e724fc3bf9b8ac6e0435e09b5
Yifan <sueyifankk@gmail.com>
2022-07-09 02:38:05 +0000

    edited add button style

--- src/main/webapp/trip/tripScript.js
+++ src/main/webapp/trip/tripScript.js
@@ -1,3 +1,32 @@
+var date = new Date();
+var tdate = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getUTCFullYear();
+if(tdate < 10){
+    tdate = '0' + tdate;
+}
+if(month < 10) {
+    month = '0' + month
+}
+var minDate = year + "-" + month + "-" + tdate
+document.getElementById("fromDate").setAttribute('min', minDate)
+
+var fromDate;
+$('#fromDate').on('change', function() {
+    fromDate = $(this).val();
+    $('#toDate').prop('min', function() {
+        return fromDate;
+    })
+});
+
+var toDate;
+$('#toDate').on('change', function() {
+    toDate = $(this).val();
+    $('#fromDate').prop('max', function() {
+        return toDate;
+    })
+});
+
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 
