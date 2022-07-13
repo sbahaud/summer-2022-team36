fc9edd0549290295fe220f91ea1c104cb3b82f9e
Emma Yuan <jinyueyu@usc.edu>
2022-07-12 22:15:43 -0700

    click trips to open details page

--- src/main/webapp/javascript/dashboard.js
+++ src/main/webapp/javascript/dashboard.js
@@ -20,13 +20,14 @@ async function fetchTrips(userId) {
     await fetch('/get-trips', {method: 'GET', headers: params}).then(response => response.json()).then((trips) => {
         trips.forEach(
             (trip) => {
-                displayTrips(trip.title, trip.totalBudget, trip.startDate, trip.endDate)});
+                displayTrips(trip.tripID, trip.title, trip.totalBudget, trip.startDate, trip.endDate)});
     });
 }   
 
-function displayTrips(title, totalBudget, startDate, endDate) {
+function displayTrips(tripId, title, totalBudget, startDate, endDate) {
     const trip = document.createElement("div");
     trip.className = "trip";
+    trip.setAttribute("onclick", "showTripDetail("+ tripId +")");
 
     var tripBox = 
         '<h3 class="trip-title">' + title + '</h3>'
@@ -37,4 +38,8 @@ function displayTrips(title, totalBudget, startDate, endDate) {
     trip.innerHTML = tripBox;
 
     document.getElementById("trips-div").appendChild(trip);
+}
+
+function showTripDetail(tripId) {
+    console.log(tripId);
 }
